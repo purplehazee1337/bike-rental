4db4b03cfb5fd5a66de7caa51859303303760218
purplehaze1337 <wjechowski@gmail.com>
2025-06-03 22:36:30 +0200

    initial

---
+++ src/test/java/pl/wit/bikerental/AppTest.java
@@ -0,0 +1,14 @@
+package pl.wit.bikerental;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
