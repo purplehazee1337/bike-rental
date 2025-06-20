d47d281447b8b1446645fa769123fdd5ba982451
Krzysztofrepo <krzysmickiewicz@gmial.com>
2025-06-20 20:38:22 +0200

    Pusta klasa testowa dla klasy Client

---
+++ src/test/java/pl/wit/bikerental/model/ClientTest.java
@@ -0,0 +1,15 @@
+package pl.wit.bikerental.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Krzysztof Mickiewicz
+ * @version 1.0
+ * @since 2025-06-15
+ */
+
+public class ClientTest {
+
+}
